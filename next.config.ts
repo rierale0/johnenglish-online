ebcdad1fcc600680089f042dc5acbccb082913c6
John Riera <johnriera395@gmail.com>
2025-03-09 05:27:06 +0100

    changed to next framework

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  appDir: true, // This enables the app directory in Next.js 13
 };
 
 export default nextConfig;
