6a98fd3715d99b862e53aa68b5d281478d40d1de
John Riera <johnriera395@gmail.com>
2025-03-09 05:33:13 +0100

    output: 'export',

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  appDir: true, // This enables the app directory in Next.js 13
-  reactStrictMode: true,
+  output: 'export',
 };
 
 export default nextConfig;
