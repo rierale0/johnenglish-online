04e47719f43e2c86538c93cedd0056b208116035
John Riera <johnriera395@gmail.com>
2025-03-09 05:40:04 +0100

    added attendance

---
+++ src/app/pycat/attendance/page.tsx
@@ -0,0 +1,11 @@
+import Form from './form';
+
+export default function Asistencia() {
+  return (
+    <div>
+      <h1>Formulario de Asistencia</h1>
+      <p>Por favor, registre su asistencia a continuación:</p>
+      <Form />
+    </div>
+  );
+}
